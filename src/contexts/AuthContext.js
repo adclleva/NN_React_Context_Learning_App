a05efb8ab0c4ea80f871a3b4c7378456f74648a5
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-16 15:12:28 -0800

    Created another context for the authorization

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,23 @@
+import React, { Component, createContext } from 'react'
+
+const { Provider, Consumer } = React.createContext()
+
+class AuthContextProvider extends Component {
+  state = {
+    isAuthenticated: false
+  }
+
+  toggleAuth = () => {
+    this.setState({ isAuthenticated: !this.state.isAuthenticated})
+  }
+
+  render() {
+    return (
+      <Provider value={{ ...this.state, toggleAuth: this.toggleAuth}}>
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+export { AuthContextProvider, Consumer as AuthContextConsumer }
