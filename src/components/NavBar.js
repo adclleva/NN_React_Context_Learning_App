443af3544ab15b77b7cc623ec0b01097c61c2d29
Arvin Lleva <arvin.lleva@yahoo.com>
2020-02-15 22:47:31 -0800

    Added the booklist and the navbar components with the css for the booklist

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+function NavBar() {
+  return (
+    <nav>
+      <h1>Context App</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar;
